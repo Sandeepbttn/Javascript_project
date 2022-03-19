00b5984630c57aa93e30ea7192d6af0bab794ec6
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-03-20 02:21:41 +0530

    updating ball script

--- Ball.js
+++ Ball.js
@@ -1,65 +1,71 @@
-const INITIAL_VELOCITY = .025
-const VELOCITY_INCREASE = .00001
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.00001
 
-export default class Ball{
-    constructor(ballElem){
-        this.ballElem = ballElem
-        this.reset()
-    }
+export default class Ball {
+  constructor(ballElem) {
+    this.ballElem = ballElem
+    this.reset()
+  }
 
-
-get x(){
+  get x() {
     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
-}
+  }
 
-set x(value){
-    this.ballElem.style.setProperty("--x",value)
-}
+  set x(value) {
+    this.ballElem.style.setProperty("--x", value)
+  }
 
-get y(){
+  get y() {
     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
-}
+  }
 
-set y(value){
-    this.ballElem.style.setProperty("--y",value)
-}
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value)
+  }
 
-
-rect() {
+  rect() {
     return this.ballElem.getBoundingClientRect()
   }
 
-reset(){
+  reset() {
     this.x = 50
     this.y = 50
-    this.direction = {x:0}
-
-    while(Math.abs(this.direction.x)<=0.2 || Math.abs(this.direction.x)>= 0.9){
-        const heading = randomNumberBtw(0,2*Math.PI)  //2*Math.PI is equal to 360 degree
-        this.direction ={ x: Math.cos(heading), y: Math.sin(heading) }
+    this.direction = { x: 0 }
+    while (
+      Math.abs(this.direction.x) <= 0.2 ||
+      Math.abs(this.direction.x) >= 0.9
+    ) {
+      const heading = randomNumberBetween(0, 2 * Math.PI)
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
     }
-
     this.velocity = INITIAL_VELOCITY
-    // console.log(this.direction)
-}
+  }
 
-update(delta){
-    this.x += this.direction.x * this.velocity *delta
-    this.y += this.direction.y * this.velocity *delta
-    this.velocity += VELOCITY_INCREASE* delta
-
+  update(delta, paddleRects) {
+    this.x += this.direction.x * this.velocity * delta
+    this.y += this.direction.y * this.velocity * delta
+    this.velocity += VELOCITY_INCREASE * delta
     const rect = this.rect()
 
-        if (rect.bottom >= window.innerHeight || rect.top <= 0) {
-            this.direction.y *= -1
-          }
-      
-          if (rect.right >= window.innerWidth || rect.left <= 0){
-            this.direction.x *= -1
-          }
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1
     }
+
+    if (paddleRects.some(r => isCollision(r, rect))) {
+      this.direction.x *= -1
+    }
+  }
 }
 
-function randomNumberBtw(min,max){
-    return Math.random()* (max-min)+ min
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left <= rect2.right &&
+    rect1.right >= rect2.left &&
+    rect1.top <= rect2.bottom &&
+    rect1.bottom >= rect2.top
+  )
 }
