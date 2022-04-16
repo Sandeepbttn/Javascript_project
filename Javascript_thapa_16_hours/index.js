3ad4c5e021397e6f414055213a4994b3d8a30974
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-16 14:01:08 +0530

    updating file

--- Javascript_thapa_16_hours/index.js
+++ Javascript_thapa_16_hours/index.js
@@ -1,1 +1,35 @@
-console.log("hello world")
+// console.log("hello world")
+
+
+// if else 
+// task
+// checking the leap year using if else
+
+// var year = 1900
+
+// if (year %4===0 || year %400===0){
+//     console.log("this year " + year + " is leap year")
+// }else{
+//     console.log("this year " + year + " is not leap year")
+// }
+
+// task 2
+// truthy and falsy values
+// 5 falsy values in JS 
+// 0,"",undefined,null,NaN,false
+
+
+
+// ternary operator
+// variablename = condition?true:false
+
+// switch statement
+// task 
+// find the area of circle, triangle and rectangle
+
+// while loop statement
+var num = 10;
+while(num<11){
+    console.log(num);
+    num++;
+}
