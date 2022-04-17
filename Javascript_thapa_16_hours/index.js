c0c862a5a7c5ac20f005007f73fdf0a26f89af78
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-17 16:17:33 +0530

    update

--- Javascript_thapa_16_hours/index.js
+++ Javascript_thapa_16_hours/index.js
@@ -28,8 +28,117 @@
 // find the area of circle, triangle and rectangle
 
 // while loop statement
-var num = 10;
-while(num<11){
-    console.log(num);
-    num++;
-}
+// var num = 10;
+// while(num<11){
+//     console.log(num);
+//     num++;
+// }
+
+
+// do-while loop
+// for loop
+// task , print table for 12
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// functions in JS
+
+// function sum(){
+//     var a=2;
+//     var b=4;
+//     var total=a+b;
+//     console.log(total);
+// }
+
+// sum()
+
+/////////////////////////////////////////// function parameter vs function arguments
+
+// function sum(a,b){
+    
+//     var total=a+b;
+//     console.log(total);
+// }
+
+// sum(8,7)
+
+
+// interviews questions
+// why we use functions?
+
+
+////////////////////////////////////// function expression
+
+// function sum(a,b){
+    
+//     var total=a+b;
+//     console.log(total);
+// }
+
+
+
+// var funcExpression = sum(8,2);
+// funcExpression;
+
+////////////////////////////////////// Return keywords
+
+// function sum(a,b){
+    
+//     return total=a+b;
+// }
+
+// var funcExpression = sum(8,2);
+// console.log(funcExpression);
+
+
+//////////////////////////////////// anonymous Function
+
+// var funcExp = function(a,b){
+//     return total=a+b;
+// }
+
+// console.log(funcExp(9,7));
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////ECMAscript or ES6///////////////////////////////////////////////////////////
+
+////// interview questions
+
+// //////////////////////////////////let vs var vs const
+
+
+//////////////////////////////////// template Literals(template strings)
+
+// for (let i=1; i<11;i++){
+//     let table=13;
+//     console.log(`${table} * ${i}= ${table * i}`);        //using template literals
+// }
+
+
+
+//////////////////////////////////// Default parameters
+
+// function mul(a,b=8){
+//     return a*b;
+// }
+
+// console.log(mul(4));
+
+
+
+
+/////////////////////////////////// fat arrow function
+
+// const sum =(a,b)=>{
+//     return a+b;
+// }
+
+// console.log(sum(4,6));
+
+/////////////////////////////////////////////////////////////ARRAY IN JAVASCRIPT//////////////////////////////////////////////////////////////////
+
+
+
